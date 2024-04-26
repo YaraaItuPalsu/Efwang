af279712dae04fcc48a82eedf447b4de4d2dd349
Rve27 <rve27github@gmail.com>
2024-04-26 23:13:07 +0000

    rvc: Update compiler script

--- rvc.sh
+++ rvc.sh
@@ -1,21 +1,74 @@
-#!/bin/bash
+#!/bin/sh
+
+DEFCONFIG="rvkernel/rvkernel_defconfig"
+CLANGDIR="/workspace/RvKernel-Builder/clang"
 
 #
-DEFCONFIG="rvkernel/rvkernel_defconfig"
+rm -rf out
 
-# you can set you name or host name(optional)
+#
+mkdir -p out
+mkdir out/RvKernel
+mkdir out/RvKernel/SE
+mkdir out/RvKernel/NSE
+
+
+#
 export KBUILD_BUILD_USER=Radika
 export KBUILD_BUILD_HOST=Rve27
+export PATH="$CLANGDIR/bin:$PATH"
 
 #
-CLANG_DIR="/workspace/RvKernel-Builder/clang-18"
-
-#
-export PATH="$CLANG_DIR/bin:$PATH"
-
-MAKE="./makeparallel"
-
-mkdir -p out
 make O=out ARCH=arm64 $DEFCONFIG
 
-make -j$(nproc --all) O=out LLVM=1 ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- 2>&1 | tee compile.log
+#
+MAKE="./makeparallel"
+
+#
+START=$(date +"%s")
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+
+rve () {
+make -j$(nproc --all) O=out LLVM=1 \
+ARCH=arm64 \
+CC=clang \
+LD=ld.lld \
+AR=llvm-ar \
+AS=llvm-as \
+NM=llvm-nm \
+OBJCOPY=llvm-objcopy \
+OBJDUMP=llvm-objdump \
+STRIP=llvm-strip \
+CROSS_COMPILE=aarch64-linux-gnu- \
+CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+}
+
+#SE
+cp RvKernel/SE/* arch/arm64/boot/dts/qcom/
+rve | tee -a compile.log
+if [ $? -ne 0 ]
+then
+    echo "Build failed"
+else
+    echo "Build succesful"
+    cp out/arch/arm64/boot/Image.gz-dtb out/RvKernel/SE/Image.gz-dtb
+    
+    #NSE
+    cp RvKernel/NSE/* arch/arm64/boot/dts/qcom/
+    rve
+    if [ $? -ne 0 ]
+    then
+        echo "Build failed"
+    else
+        echo "Build succesful"
+        cp out/arch/arm64/boot/Image.gz-dtb out/RvKernel/NSE/Image.gz-dtb
+    fi
+fi
+
+END=$(date +"%s")
+DIFF=$(($END - $START))
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
