2be7a60ce848a3c954be2bbcb0475dde2f45fe08
Rve27 <rve27github@gmail.com>
2024-05-21 02:48:48 +0700

    boeffla_wl_blocker: Increase wakelock list length

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -18,6 +18,6 @@
 
 #define LIST_WL_DEFAULT				"qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK;898000.qcom,qup_uart;IPA_WS;wlan_ipa;wlan_pno_wl;wcnss_filter_lock;hal_bluetooth_lock;13930000.decon_fb;ssp_comm_wake_lock;GPSD;ssp_wake_lock;tsp_wakelock;umts_ipc0;umts_ipc1;mmc0_detect;;qpnp_fg_memaccess;qpnp_fg_update_sram;[timerfd];sensor_ind;PowerManager.SuspendLockout;IPA_RM12"
 
-#define LENGTH_LIST_WL				255
-#define LENGTH_LIST_WL_DEFAULT		100
+#define LENGTH_LIST_WL				1024
+#define LENGTH_LIST_WL_DEFAULT		114
 #define LENGTH_LIST_WL_SEARCH		LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5
